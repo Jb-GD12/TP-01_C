db4f8b48ab92250ba1cd9d74ea7975cf99ecb423
Alex-GD12 <59962597+Alex-GD12@users.noreply.github.com>
2020-01-25 14:06:21 +0100

    Create Branche02.c

---
+++ Branche02.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//Schéma des différentes fonctionnalités du rendu final
+
+// Fonction d'attaque
+
+int attaque (pv, atq, def){
+	if (tamponDef = 1){
+		pv -= atqJoueur/defEnnemi;
+	}else{
+		pv -= atqjoueur;
+		
+	}
+}
+
+// Fonction gain xp
+
+int gainExp ( xpJoueur, xpMonstre, xpMax, lvl){
+	xpJoueur += xpMonstre;
+	while(xpJoueur >= xpMax){
+		xpJoueur -= xpMax;
+		lvl += 1;
+		xpMax += 10;
+		
+		atq += 2;
+		PV += 5;
+		pm += 5;
+		
+		
+	}
+}
+
+
